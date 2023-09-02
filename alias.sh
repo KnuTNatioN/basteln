2dd39159c9f20bb6b7c5bedf6a4bd0bc0036ba9c
KnuT <ssx1337ssy@gmail.com>
2023-09-02 21:36:53 +0200

    Update alias.sh
    updated aliases

--- alias.sh
+++ alias.sh
@@ -3,7 +3,7 @@
 bashalias() {
 # get bash aliases
 echo -e "\e[101m...\e[0m \e[38;5;82m hole nun die aliases\e[0m"
-wget https://gist.githubusercontent.com/KnuTNatioN/d52e83b281f049812d76ea925cbea5d3/raw/b221ec3ea456169e1cdb9d359ee8e676c25bc26b/myalias
+wget https://gist.githubusercontent.com/KnuTNatioN/854dbf07c6b21dcbde8be0a5f34e2c00/raw/e28f3703a79da4ca5490db77b9e878acb7f9c4f0/.bash_aliases
 cat myalias >> /home/pi/.bash_aliases
 rm myalias
 chown pi:pi .bash_aliases
